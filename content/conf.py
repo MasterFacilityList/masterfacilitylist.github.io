376cbe91e38360f7560869dfd5b477d3c265d3ea
Chomba Ng'ang'a <chomba@savannahinformatics.com>
2015-05-11 11:59:21 +0300

    Remove all instaces of extra 'documentation'

--- content/conf.py
+++ content/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# MasterFacilityList Documentation documentation build configuration file, created by
+# MasterFacilityList documentation build configuration file, created by
 # sphinx-quickstart on Mon May 11 11:41:14 2015.
 #
 # This file is execfile()d with the current directory set to its
@@ -222,7 +222,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'MasterFacilityListDocumentation.tex', u'MasterFacilityList Documentation Documentation',
+  (master_doc, 'MasterFacilityListDocumentation.tex', u'MasterFacilityList Documentation',
    u'SIL', 'manual'),
 ]
 
@@ -252,7 +252,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'masterfacilitylistdocumentation', u'MasterFacilityList Documentation Documentation',
+    (master_doc, 'masterfacilitylistdocumentation', u'MasterFacilityList Documentation',
      [author], 1)
 ]
 
@@ -266,7 +266,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'MasterFacilityListDocumentation', u'MasterFacilityList Documentation Documentation',
+  (master_doc, 'MasterFacilityListDocumentation', u'MasterFacilityList Documentation',
    author, 'MasterFacilityListDocumentation', 'One line description of project.',
    'Miscellaneous'),
 ]
